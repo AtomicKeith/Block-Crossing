12458f879021d80033bb4b594c165360057f7e5a
Keith__ <trenton.ingleshead@gmail.com>
2024-08-25 16:29:39 -0500

    Initial commit

---
+++ src/main/java/net/atomickeith/blockcrossing/BlockCrossingDataGenerator.java
@@ -0,0 +1,11 @@
+package net.atomickeith.blockcrossing;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlockCrossingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/atomickeith/blockcrossing/BlockCrossingModClient.java
@@ -0,0 +1,10 @@
+package net.atomickeith.blockcrossing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockCrossingModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
